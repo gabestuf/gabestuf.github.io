50eb1765693dc2b149bad0b346a1ebf97ea07b22
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 19:30:20 -0500

    More testing and replaced contacts with Zeros

--- js/clicker.js
+++ js/clicker.js
@@ -1,41 +1,44 @@
-const buttonElement = document.getElementById('btn')
-const homeButtonElement = document.getElementById('home-btn')
-const counterElement = document.getElementById('counter-btn')
-
+const buttonElement = document.getElementById("btn");
+const homeButtonElement = document.getElementById("home-btn");
+const counterElement = document.getElementById("counter-btn");
 
 //Sets counter variable. If counter is in local storage, sets value to that, otherwise if undefined or counter is not a number, resets to 0.
-if (!isNaN(parseInt(localStorage.getItem('counter')))) {
-    var counter = counterElement.innerText = parseInt(localStorage.getItem('counter'));
+if (!isNaN(parseInt(localStorage.getItem("counter")))) {
+  var counter = (counterElement.innerText = parseInt(
+    localStorage.getItem("counter")
+  ));
 } else {
-    var counter = 0;
-    localStorage.setItem('counter', 0);
+  var counter = 0;
+  localStorage.setItem("counter", 0);
 }
 
 //
 function buttonClick() {
-    buttonElement.addEventListener('click', updateNumber)
-    homeButtonElement.addEventListener('click', homePage)
+  buttonElement.addEventListener("click", updateNumber);
+  homeButtonElement.addEventListener("click", homePage);
 }
 //Updates counter variable, label, and storage. Updates label and stores counter in localStorage.
 function updateNumber() {
-    //makes sure counter is in local storage and is a number. otherwise resets counter.
-    if (!isNaN(parseInt(localStorage.getItem('counter')))) {
-        var counter = counterElement.innerText = parseInt(localStorage.getItem('counter'));
-    } else {
-        var counter = 0;
-        localStorage.setItem('counter', 0);
-    }
+  //makes sure counter is in local storage and is a number. otherwise resets counter.
+  if (!isNaN(parseInt(localStorage.getItem("counter")))) {
+    var counter = (counterElement.innerText = parseInt(
+      localStorage.getItem("counter")
+    ));
+  } else {
+    var counter = 0;
+    localStorage.setItem("counter", 0);
+  }
 
-    counter += 1;
-    localStorage.setItem('counter', counter);
-    counterElement.innerText = parseInt(localStorage.getItem('counter'));
+  counter += 1;
+  localStorage.setItem("counter", counter);
+  counterElement.innerText = parseInt(localStorage.getItem("counter"));
 }
 
 function homePage() {
-    window.location.href = 'index.html';
+  window.location.href = "index.html";
 }
 
-buttonClick()
+buttonClick();
 
 // localStorage.setItem('name', 'joe mama');
 // localStorage.setItem('age', '69');
@@ -46,33 +49,72 @@ buttonClick()
 
 // console.log(localStorage.getItem(localStorage.key(1)));
 
-const inpKey = document.getElementById('inpKey');
-const inpValue = document.getElementById('inpValue');
-const btnInsert = document.getElementById('btnInsert');
-const lsOutput = document.getElementById('lsOutput');
-const btnClear = document.getElementById('btnClear');
+const inpKey = document.getElementById("inpKey");
+const inpValue = document.getElementById("inpValue");
+const btnInsert = document.getElementById("btnInsert");
+const lsOutput = document.getElementById("lsOutput");
+const btnClear = document.getElementById("btnClear");
 
-btnInsert.onclick = function() {
-    const key = inpKey.value;
-    const value = inpValue.value;
-    //console.log(key);
-    //console.log(value);
+btnInsert.onclick = function () {
+  const key = inpKey.value;
+  const value = inpValue.value;
+  //console.log(key);
+  //console.log(value);
 
-    //checks if empty
-    if (key && value) {
-        localStorage.setItem(key, value);
-        location.reload();
-    }
-}
+  //checks if empty
+  if (key && value) {
+    localStorage.setItem(key, value);
+    location.reload();
+  }
+};
 
 for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
 
-    lsOutput.innerHTML += `${key}: ${value}<br />`;
+  lsOutput.innerHTML += `${key}: ${value}<br />`;
 }
 
-btnClear.onclick = function() {
-    localStorage.clear();
-    location.reload();
+btnClear.onclick = function () {
+  localStorage.clear();
+  location.reload();
+};
+
+
+
+
+
+
+
+
+const colors = [
+  '#2196f3',
+  '#e91e63',
+  '#ffeb3b',
+  '#74ff1d'
+]
+
+
+function createSquare() {
+  const section = document.getElementById('blackgrond');
+  const square = document.createElement('span');
+
+  var size = Math.random() * 50;
+  square.style.width = 20 + size +'px';
+  square.style.height = 20 + size +'px';
+
+  square.style.top = Math.random() * innerHeight + 'px';
+  square.style.left = Math.random() * innerWidth + 'px';
+
+  const bg = colors[Math.floor(Math.random() * colors.length)];
+  square.style.background = bg;
+
+  section.appendChild(square);
+
+  setTimeout(() =>{
+      square.remove()
+  }, 5000)
 }
+
+setInterval(createSquare, 150)
+
--- js/test.js
+++ js/test.js
@@ -1,15 +1,21 @@
-var tl2 = new TimelineMax();
-const controller = new ScrollMagic.Controller();
+const input = document.getElementById('input');
+const buttonElement = document.getElementById('btn');
+const output = document.getElementById('output');
 
+const inp = input.nodeValue;
 
+function buttonClick() {
+    buttonElement.addEventListener("click", printNumber);
+  }
 
-tl2.from("#box", 1, { opacity: 0, scale: 0 });
-tl2.to("#box", .5, { left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)' })
+function printNumber() {
+    console.log(parseInt(input.value));
+    const num = parseInt(input.value);
+    var str = "";
+    for (let i = 0; i < num; i++) {
+        str += "0";
+      }
+      output.innerHTML += `<p>${str}<p>`;
+}
 
-
-
-const scene2 = new ScrollMagic.Scene({
-        triggerElement: "blockquote"
-    })
-    .setTween(tl2)
-    .addTo(controller);
+buttonClick();
