91e2ceaab3653f73cbfe90dbe2847e2a32b4a117
BuildTools <unconfigured@null.spigotmc.org>
2021-03-10 16:43:18 -0500

    Put html in folder, linked docs back

---
+++ randomstuf/zeromaker.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('input');
+const buttonElement = document.getElementById('btn');
+const output = document.getElementById('output');
+
+const inp = input.nodeValue;
+
+function buttonClick() {
+    buttonElement.addEventListener("click", printNumber);
+  }
+
+function printNumber() {
+    console.log(parseInt(input.value));
+    const num = parseInt(input.value);
+    var str = "";
+    for (let i = 0; i < num; i++) {
+        str += "0";
+      }
+      output.innerHTML = `<p>${str}<p>`;
+}
+
+buttonClick();
