8eef0401dfbb74abd9c967f14fcf0cf6a00bb14c
Gabriel Camacho <gabestuf@gmail.com>
2021-07-02 20:56:51 -0400

    starfield

--- src/js/test.js
+++ src/js/test.js
@@ -1,29 +1,33 @@
-const STAR_COUNT = 1000;
-
-
-
-
-
-
+const STAR_COUNT = 420;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const CANVAS = document.querySelector('.background-container');
-const c = CANVAS.getContext('2d');
-//canvas.width = window.innerWidth; //screen width
-//canvas.height = window.innerHeight; //screen height
+const CANVAS = document.querySelector(".background-container");
 
 for (let x = 0; x < STAR_COUNT; x++) {
   const size = getRandomInt(3) + 1;
-  const div = document.createElement('div');
+  const div = document.createElement("div");
+  switch (size) {
+    case 1:
+      div.classList.add("star1");
+      break;
+    case 2:
+      div.classList.add("star2");
+      break;
+    case 3:
+      div.classList.add("star3");
+      break;
+    default:
+      break;
+  }
+  div.classList.add("star")
   div.style.width = `${size}px`;
   div.style.height = `${size}px`;
-  div.style.backgroundColor = "white";
-  div.style.position = 'absolute';
+  div.style.position = "absolute";
 
-  const X_POS = getRandomInt(10000) / 100;
+  const X_POS = getRandomInt(30000) / 100 - 100;
   const Y_POS = getRandomInt(10000) / 100;
 
   div.style.marginLeft = `${X_POS}vw`;
@@ -31,4 +35,3 @@ for (let x = 0; x < STAR_COUNT; x++) {
 
   CANVAS.appendChild(div);
 }
-
