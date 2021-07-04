ffc5c94bc3d3cd7293f8073ca8464f4911f477bb
Gabriel Camacho <gabestuf@gmail.com>
2021-07-04 02:46:00 -0400

    starfield finally working

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,4 @@
-const STAR_COUNT = 420;
+const STAR_COUNT = 1000;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -9,29 +9,41 @@ const CANVAS = document.querySelector(".background-container");
 for (let x = 0; x < STAR_COUNT; x++) {
   const size = getRandomInt(3) + 1;
   const div = document.createElement("div");
-  switch (size) {
-    case 1:
-      div.classList.add("star1");
-      break;
-    case 2:
-      div.classList.add("star2");
-      break;
-    case 3:
-      div.classList.add("star3");
-      break;
-    default:
-      break;
-  }
-  div.classList.add("star")
   div.style.width = `${size}px`;
   div.style.height = `${size}px`;
   div.style.position = "absolute";
+  div.classList.add(`star${size}`);
 
-  const X_POS = getRandomInt(30000) / 100 - 100;
-  const Y_POS = getRandomInt(10000) / 100;
+  let X_POS, Y_POS;
 
-  div.style.marginLeft = `${X_POS}vw`;
-  div.style.marginTop = `${Y_POS}vh`;
+  switch (x % 3) {
+    case 0:
+      //console.log("0");
+      X_POS = getRandomInt(10000) / 100;
+      Y_POS = getRandomInt(10000) / 100;
+      div.classList.add("midpos");
+      break;
+    case 1:
+      //console.log("1");
+      X_POS = getRandomInt(10000) / 100 + 100;
+      Y_POS = getRandomInt(10000) / 100;
+      div.classList.add("leftpos1");
+      break;
+    case 2:
+      //console.log("2");
+      X_POS = getRandomInt(10000) / 100 + 100;
+      Y_POS = getRandomInt(10000) / 100;
+      div.classList.add("leftpos2");
+      break;
+    default:
+      console.log("switch case broke");
+      break;
+  }
+
+  div.classList.add("star");
+
+  div.style.right = `${X_POS}vw`;
+  div.style.top = `${Y_POS}vh`;
 
   CANVAS.appendChild(div);
 }
