2284a808c701e5247e2a848294a9e9dde8493a5a
Gabriel Camacho <gabestuf@gmail.com>
2021-07-04 20:33:25 -0400

    more stars

--- src/js/test.js
+++ src/js/test.js
@@ -9,8 +9,8 @@ const CANVAS = document.querySelector(".background-container");
 for (let x = 0; x < STAR_COUNT; x++) {
   const size = getRandomInt(3) + 1;
   const div = document.createElement("div");
-  div.style.width = `${size}px`;
-  div.style.height = `${size}px`;
+  div.style.width = `${size+1}px`;
+  div.style.height = `${size+1}px`;
   div.style.position = "absolute";
   div.classList.add(`star${size}`);
 
@@ -19,21 +19,20 @@ for (let x = 0; x < STAR_COUNT; x++) {
   switch (x % 3) {
     case 0:
       //console.log("0");
-      X_POS = getRandomInt(10000) / 100;
+      X_POS = getRandomInt(10000) / 100 + 100;
       Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("midpos");
+      div.classList.add("leftpos1");
       break;
     case 1:
       //console.log("1");
       X_POS = getRandomInt(10000) / 100 + 100;
       Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("leftpos1");
+      div.classList.add("leftpos2");
       break;
     case 2:
-      //console.log("2");
-      X_POS = getRandomInt(10000) / 100 + 100;
+      X_POS = getRandomInt(10000) / 100;
       Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("leftpos2");
+      div.classList.add("midpos");
       break;
     default:
       console.log("switch case broke");
@@ -47,3 +46,22 @@ for (let x = 0; x < STAR_COUNT; x++) {
 
   CANVAS.appendChild(div);
 }
+
+
+
+
+let root = document.documentElement;
+
+
+const btn = document.querySelector('.reverse')
+btn.addEventListener('click', () => {
+  root.style.setProperty('--animation-direction', 'reverse')
+})
+
+const btn2 = document.querySelector('.speed')
+btn2.addEventListener('click', () => {
+  root.style.setProperty('--speed', '.01s')
+})
+
+
+//setInterval(updateStarPosition, 1000);
