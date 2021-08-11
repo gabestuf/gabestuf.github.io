9b3d54bd9d01ac561724e0b0e3e36ecfffce4e9d
Gabriel Camacho <unconfigured@null.spigotmc.org>
2021-08-11 17:30:41 -0400

    Removed Border

--- src/js/moving-stars.js
+++ src/js/moving-stars.js
@@ -1,50 +1,48 @@
 const STAR_COUNT = 690;
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 
 const CANVAS = document.querySelector(".background-container");
 
 for (let x = 0; x < STAR_COUNT; x++) {
-  const size = getRandomInt(3) + 1;
-  const div = document.createElement("div");
-  div.style.width = `${size+1}px`;
-  div.style.height = `${size+1}px`;
-  div.style.position = "absolute";
-  div.classList.add(`star${size}`);
+    const size = getRandomInt(3) + 1;
+    const div = document.createElement("div");
+    div.style.width = `${size + 1}px`;
+    div.style.height = `${size + 1}px`;
+    div.style.position = "absolute";
+    div.classList.add(`star${size}`);
 
-  let X_POS, Y_POS;
+    let X_POS, Y_POS;
 
-  switch (x % 3) {
-    case 0:
-      //console.log("0");
-      X_POS = (getRandomInt(100000) / 1000) + 100;
-      Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("leftpos1");
-      break;
-    case 1:
-      //console.log("1");
-      X_POS = (getRandomInt(100000) / 1000) + 100;
-      Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("leftpos2");
-      break;
-    case 2:
-      X_POS =(getRandomInt(100000) / 1000);
-      Y_POS = getRandomInt(10000) / 100;
-      div.classList.add("midpos");
-      break;
-    default:
-      console.log("switch case broke");
-      break;
-  }
+    switch (x % 3) {
+        case 0:
+            X_POS = (getRandomInt(100000) / 1000) + 100;
+            Y_POS = getRandomInt(10000) / 100;
+            div.classList.add("leftpos1");
+            break;
+        case 1:
+            X_POS = (getRandomInt(100000) / 1000) + 100;
+            Y_POS = getRandomInt(10000) / 100;
+            div.classList.add("leftpos2");
+            break;
+        case 2:
+            X_POS = (getRandomInt(100000) / 1000);
+            Y_POS = getRandomInt(10000) / 100;
+            div.classList.add("midpos");
+            break;
+        default:
+            console.log("switch case broke");
+            break;
+    }
 
-  div.classList.add("star");
+    div.classList.add("star");
 
-  div.style.right = `${X_POS}%`;
-  div.style.top = `${Y_POS}vh`;
+    div.style.right = `${X_POS}vw`;
+    div.style.top = `${Y_POS}vh`;
 
-  CANVAS.appendChild(div);
+    CANVAS.appendChild(div);
 }
 
 
--- src/js/test2.js
+++ src/js/test2.js
@@ -1,29 +1,29 @@
 var pageState = 1;
 
 $(function () {
-    
+
     $('#btn-1').on("click", () => {
         if (pageState != 1) {
-            $('.main').animate({left: "0vw"}, 1000)
-                      .animate({top: "0px"});
+            $('.main').animate({ left: "0vw" }, 700)
+                .animate({ top: "0px" });
             pageState = 1;
         }
     });
     $('#btn-2').on("click", () => {
         if (pageState != 2) {
-            $('.main').animate({left: "-100vw", top: '0px'}, 1000);
+            $('.main').animate({ left: "-100vw", top: '0px' }, 700);
             pageState = 2;
-        } 
+        }
     });
     $('#btn-3').on("click", () => {
         if (pageState != 3) {
-            $('.main').animate({left: "-200vw"}, 1000);
+            $('.main').animate({ left: "-200vw" }, 700);
             pageState = 3;
         }
     });
     $('#btn-4').on("click", () => {
         if (pageState != 4) {
-            $('.main').animate({left: "-300vw", top: '0px'}, 1000);
+            $('.main').animate({ left: "-300vw", top: '0px' }, 700);
             pageState = 4;
         }
     });
