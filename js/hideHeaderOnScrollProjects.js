2c05d5b10fe51f00326c72074a65f8abe1549f58
Gabe Camacho <gabestuf@gmail.com>
2022-03-08 23:19:51 -0500

    somewhat functional website

--- js/hideHeaderOnScrollProjects.js
+++ js/hideHeaderOnScrollProjects.js
@@ -1,6 +1,7 @@
 
 
 
+const hamburgerElement = document.querySelector(".hamburger")
 
 const cardWrapperElement = document.querySelector(".card-wrapper")
 const desktopHeaderElement = document.querySelector(".desktop-header")
@@ -8,17 +9,32 @@ const desktopHeaderElement = document.querySelector(".desktop-header")
 cardWrapperElement.onscroll = () => {hideOnScroll()}
 
 const hideOnScroll = () => {
-    if (cardWrapperElement.scrollTop < 10) {
-        if (desktopHeaderElement.classList.contains("closed-header")) {
-            desktopHeaderElement.classList.remove("closed-header")
-        }
+    if (cardWrapperElement.scrollTop == 0) {
+        autoShowDesktopHeader();
     } else {
-        desktopHeaderElement.classList.add("closed-header")
+        if (hamburgerElement.dataset.manualopen == 0) {
+            autoHideDesktopHeader();
+        }
     }
   }
 
-  desktopHeaderElement.onclick = () => {
-      if (desktopHeaderElement.classList.contains("closed-header")) {
+
+  const autoHideDesktopHeader = () => {
+    desktopHeaderElement.classList.add("closed-header") 
+}
+  const hideDesktopHeader = () => {
+        desktopHeaderElement.classList.add("closed-header") 
+        hamburgerElement.dataset.manualopen = 0
+  }
+  
+  const autoShowDesktopHeader = () => {
+    if (desktopHeaderElement.classList.contains("closed-header")) {
         desktopHeaderElement.classList.remove("closed-header")
+      }
+  }
+  const showDesktopHeader = () => {
+    if (desktopHeaderElement.classList.contains("closed-header")) {
+        desktopHeaderElement.classList.remove("closed-header")
+        hamburgerElement.dataset.manualopen = 1
       }
   }
--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 
-
 const mobileMenuElement = document.querySelector(".nav-wrapper")
 const mobileMenuButtonElement = document.querySelector(".menu-button")
 const contentElement = document.querySelector(".content-wrapper")
-
-
+const aboutWrapperElement = document.querySelector(".about-wrapper")
 
 window.onload = () => {
     mobileMenuElement.classList.add('hideElement');
@@ -13,19 +11,24 @@ window.onload = () => {
 }
 
 const toggleDisplayMenu = () => {
- 
     if (mobileMenuElement.dataset.menuopen == 1) { // if mobileMenu is been closed
         mobileMenuElement.dataset.menuopen = 0;
         mobileMenuElement.classList.add('hideElement');
         contentElement.classList.remove('hideElement');
         mobileMenuButtonElement.classList.add('color-light');
-        mobileMenuButtonElement.classList.remove('border-dark');
+        mobileMenuButtonElement.classList.remove('color-white');
+        if (aboutWrapperElement){
+            aboutWrapperElement.classList.remove('hideElement')
+        }
     } else if (mobileMenuElement.dataset.menuopen == 0) { // mobile menu open
         mobileMenuElement.dataset.menuopen = 1;
         mobileMenuElement.classList.remove('hideElement');
         contentElement.classList.add('hideElement');
         mobileMenuButtonElement.classList.remove('color-light');
-        mobileMenuButtonElement.classList.add('border-dark');
+        mobileMenuButtonElement.classList.add('color-white');
+        if (aboutWrapperElement){
+            aboutWrapperElement.classList.add('hideElement')
+        }
     }
 }
 
