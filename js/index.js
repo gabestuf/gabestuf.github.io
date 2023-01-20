f4a76abf63277dc8043727488623fda3d685b567
Gabe Camacho <gabestuf@gmail.com>
2023-01-20 01:40:34 -0500

    removed footer, fixed menu popup functionality on home page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const mobileMenuElement = document.querySelector(".nav-wrapper")
 const mobileMenuButtonElement = document.querySelector(".menu-button")
 const contentElement = document.querySelector(".content-wrapper")
 const aboutWrapperElement = document.querySelector(".about-wrapper")
-const footerElement = document.getElementById('footer')
+// const footerElement = document.getElementById('footer')
 
 window.onload = () => {
     mobileMenuElement.classList.add('hideElement');
@@ -19,7 +19,7 @@ const toggleDisplayMenu = () => {
         contentElement.classList.remove('hideElement');
         mobileMenuButtonElement.classList.add('color-light');
         mobileMenuButtonElement.classList.remove('color-white');
-        footerElement.classList.remove('hideElement')
+        // footerElement.classList.remove('hideElement')
         if (aboutWrapperElement) {
             aboutWrapperElement.classList.remove('hideElement')
         }
@@ -29,7 +29,7 @@ const toggleDisplayMenu = () => {
         contentElement.classList.add('hideElement');
         mobileMenuButtonElement.classList.remove('color-light');
         mobileMenuButtonElement.classList.add('color-white');
-        footerElement.classList.add('hideElement')
+        // footerElement.classList.add('hideElement')
         if (aboutWrapperElement) {
             aboutWrapperElement.classList.add('hideElement')
         }
