a3c63efa3f40474f1060cb511b70ec83164cfb16
Gabe Camacho <gabestuf@gmail.com>
2023-02-16 01:04:01 -0500

    started working on contact section, changed some css

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,10 @@
+// const contactForm = document.querySelector('.contact-form')
+
+// contactForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     console.log("E")
+// })
+
 
 
 
